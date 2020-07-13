4c37a41104166208548d4bab89e7f2de76f170d3
Arun Kumar Selvaraj <dev@ioak.org>
2020-07-13 23:31:35 +0530

    Copyright section

--- src/components/CopyrightSection/index.tsx
+++ src/components/CopyrightSection/index.tsx
@@ -6,14 +6,14 @@ import { Link } from "gatsby"
 export default function CopyrightSection() {
   return (
     <div className="copyright-section">
-      <div className="copyright-section--copyright">
+      <div className="copyright-section--copyright desktop-only">
         Copyright © 2020 ioak.org
       </div>
-      <i className="fa fa-circle desktop-only" aria-hidden="true" />
+      <span className="desktop-only">|</span>
       <Link to="/terms" title="Terms and conditions">
         Terms and conditions
       </Link>
-      <i className="fa fa-circle desktop-only" aria-hidden="true" />
+      <span>|</span>
       <Link to="/privacy" title="Privacy policy">
         Privacy policy
       </Link>
