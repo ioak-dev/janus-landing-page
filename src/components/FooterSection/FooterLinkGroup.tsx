6a031da80c3e13bd29946242bccaacee09a1d869
Arun Kumar Selvaraj <dev@ioak.org>
2020-07-13 22:04:47 +0530

    SEO optimization

--- src/components/FooterSection/FooterLinkGroup.tsx
+++ src/components/FooterSection/FooterLinkGroup.tsx
@@ -18,7 +18,7 @@ export default function FooterLinkGroup(props: Props) {
       <div className="footer-link-group--title">{props.data.group}</div>
       <div className="footer-link-group--content">
         {props.data.list.map((item, index) => (
-          <Link to={item.href} key={index}>
+          <Link to={item.href} key={index} title={item.label}>
             {item.label}
           </Link>
         ))}
